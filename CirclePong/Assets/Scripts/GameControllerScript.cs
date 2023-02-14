27fa807f0ba1c3c93928a90d408510235c47bab0
chrs wilson <chris.a.wilson1234@gmail.com>
2023-02-13 23:18:40 -0500

    Can be played now (kinda)
    Game controller scrip can now spawn a new ball when a point is scored and the player who receives the ball can serve it with spacebar.

--- CirclePong/Assets/Scripts/GameControllerScript.cs
+++ CirclePong/Assets/Scripts/GameControllerScript.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 // Unfinished
 
@@ -9,10 +11,19 @@ public class GameControllerScript : MonoBehaviour
     // Public variables. Can be referenced with GameObject.Find ("GameController").GetComponent <GameControllerScript>().variableName;
     public int p1Score;
     public int p2Score;
-    public int timer=69;
+    public float timer;
     public int timeLimit;
     public int scoreLimit;
     public float speedUpRate = 1.01f;
+    public Vector3 ballServeOffset;
+    public GameObject BallPrefab;
+    public GameObject P1Paddle;
+    public GameObject P2Paddle;
+    public GameObject P1Track;
+    public GameObject P2Track;
+    private GameObject BallClone;
+    private float currentTime;
+    private int isParentedTo;
 
 
     void Start()
@@ -20,12 +31,14 @@ public class GameControllerScript : MonoBehaviour
         // Initialize scores and timer when game starts
         p1Score = 0;
         p2Score = 0;
-        timer = 0; 
+        timer = 0;
+        currentTime = timeLimit;
     }
 
     // ScorePoint function. Can be called with -- GameObject.Find ("GameController").GetComponent <GameControllerScript>().ScorePoint(isPlayer1); -- Replace "isPlayer1" with true or false.
     public void ScorePoint(bool isPlayer1)
     {
+    // Add point to respective player
         // If Player 1
         if (isPlayer1 == true)
         {
@@ -38,16 +51,53 @@ public class GameControllerScript : MonoBehaviour
             // Add 1 to p2Score
             p2Score ++;
         }
-        Debug.Log("P1: " + GameObject.Find ("GameController").GetComponent <GameControllerScript>().p1Score + "P2: " + GameObject.Find ("GameController").GetComponent <GameControllerScript>().p2Score);
+    // Check if players reached max score, go to end screen for player if so and make new ball if not
+        // If player 1 reached score limit
+        if(p1Score >= scoreLimit)
+        {
+            // Go to player 1 win scene
+        }
+        // Else if player 2 reached score limit
+        else if (p2Score >= scoreLimit)
+        {
+            // Go to player 2 win scene
+        }
+        // Else (no player hit score limit)
+        else
+        {
+        // Make a new ball as child of scoring player
+            if (isPlayer1 == true)
+            {
+                BallClone = Instantiate(BallPrefab, P1Paddle.transform.position, P1Paddle.transform.rotation, P1Track.transform);
+                isParentedTo = 1;
+            }
+            else if (isPlayer1 == false)
+            {
+               BallClone = Instantiate(BallPrefab, P2Paddle.transform.position, P1Paddle.transform.rotation, P2Track.transform);
+               isParentedTo = 2;
+            }
+            // Do stuff to new ball 
+            Rigidbody NewBallRB = BallClone.GetComponent<Rigidbody>();
+            NewBallRB.velocity = Vector3.zero;
+            NewBallRB.isKinematic = true;
+            BallClone.GetComponent<Serve>().isParentedTo = isParentedTo;
+        }
 
-        // End game if score limit is reached
-
-        // Make a new ball if score limit isn't reached
+        
     }
 
     void Update()
     {
-        
+        //Decrement currentTime
+        currentTime -= 1 * Time.deltaTime;
+        //Round to int and set timer to result
+        timer =  (int)Math.Round(currentTime, 0);
+
+        if(timer <= 0)
+        {
+            Debug.Log("Game Over");
+            //End Game
+        }
     }
 
 }
--- CirclePong/Assets/Scripts/Serve.cs
+++ CirclePong/Assets/Scripts/Serve.cs
@@ -7,19 +7,43 @@ public class Serve : MonoBehaviour
     private Vector3 bounceVelocity;
     public Rigidbody RB;
     public float serveSpeed;
+    public int isParentedTo;
+    public KeyCode serveButton;
  
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        RB.velocity = new Vector3(serveSpeed, 0, 0);
+        if (isParentedTo == 0)
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3 (serveSpeed, 0, 0);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isParentedTo == 1)
+        {
+            if (Input.GetKey(serveButton))
+            {
+                GetComponent<Transform>().parent = null;
+                GetComponent<Rigidbody>().isKinematic = false;
+                GetComponent<Rigidbody>().velocity = serveSpeed * Vector3.Normalize(GameObject.Find ("Track1").transform.position - GameObject.Find("Player1").transform.position);
+                isParentedTo = 0;
+            }
+        }
+
+        else if (isParentedTo == 2)
+        {
+            if (Input.GetKey(serveButton))
+            {
+                GetComponent<Transform>().parent = null;
+                GetComponent<Rigidbody>().isKinematic = false;
+                GetComponent<Rigidbody>().velocity = serveSpeed * Vector3.Normalize(GameObject.Find ("Track1").transform.position - GameObject.Find("Player2").transform.position);
+                isParentedTo = 0;
+            }
+        }
     }
 }
